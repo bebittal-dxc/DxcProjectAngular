c1c671221d42f0de3210f6d4eb4d856c74252a17
- <bebittalkb@dxc.com>
2020-09-27 13:29:09 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http';  
+import {DataTableModule} from 'angular-datatable';  
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent,
+    StudentListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule,  
+    DataTableModule  
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
